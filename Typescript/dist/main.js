c436f8edbe9eb4311c67923167405b47d371c075
Akanksha Patel <a.patel@easternenterprise.com>
2021-09-06 18:19:40 +0530

    created config file and declare variable and function in TS

---
+++ Typescript/dist/main.js
@@ -0,0 +1,7 @@
+//variable
+var variable = "hello world";
+//function
+var getFullName = function (name, surname) {
+    return name + " " + surname;
+};
+console.log(getFullName("Akanksha", "patel"));
---
+++ Typescript/src/main.ts
@@ -0,0 +1,9 @@
+//variable
+let variable: string = "hello world" 
+
+//function
+const getFullName = (name: string, surname: string): string => {
+    return name + " " + surname;
+}
+
+console.log(getFullName("Akanksha","patel"));
