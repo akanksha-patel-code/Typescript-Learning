8499c5037dcf94ff7f901b4173c9357a5f8910b7
Akanksha Patel <a.patel@easternenterprise.com>
2021-09-06 18:31:30 +0530

    created objects in TS

---
+++ Typescript/dist/user.js
@@ -0,0 +1,10 @@
+//Objects
+var user = {
+    name: 'Akanksha',
+    age: 26
+};
+//Duplicating types here. Better approch is to create interface
+var user2 = {
+    name: "jack",
+    age: 20
+};
---
+++ Typescript/src/user.ts
@@ -0,0 +1,12 @@
+//Objects
+const user: {name: string, age:number} = {
+    name: 'Akanksha',
+    age: 26
+}
+
+
+//Duplicating types here. Better approch is to create interface
+const user2: {name: string, age:number} = {
+    name: "jack",
+    age: 20
+}
