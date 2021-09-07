0bc90ca8d862e16a6f9f0d4300c21b31277c4209
Akanksha Patel <a.patel@easternenterprise.com>
2021-09-07 13:39:13 +0530

    DOM in ts

---
+++ Typescript/dist/dom.js
@@ -0,0 +1,3 @@
+//DOM
+var someElement = document.querySelector(".foo");
+console.log("someElement", someElement.value);
---
+++ Typescript/src/dom.ts
@@ -0,0 +1,3 @@
+//DOM
+const someElement = document.querySelector(".foo") as HTMLInputElement;
+console.log("someElement", someElement.value);
